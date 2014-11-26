59e97f6dbee06de4a61bf42347300a892b0077b4
gsimpson <gs@bn.co>
2014-11-26 00:10:44 -0500

    Changing mobile endpoint config.

--- apps/api/utils/config.js
+++ apps/api/utils/config.js
@@ -11,7 +11,12 @@ config = {
   MODE: env.MODE || 'development',
   TOKEN: env.TOKEN || 'token',
   ELASTIC_SEARCH_APIS: env.ELASTIC_SEARCH_APIS || [ 'http://localhost:9200' ],
-  PORT: env.PORT || 3000
+  PORT: env.PORT || 3002,
+  NOTIFICATION_URL: env.NOTIFICATION_URL || '',
+  AWS_ACCESSKEYID: env.AWS_ACCESSKEYID || '',
+  AWS_SECRETACCESSKEY: env.AWS_SECRETACCESSKEY || '',
+  SNS_ARN: env.SNS_ARN || 'arn:aws:sns:us-west-2:036845378506:Mobile_Publication'
+
 };
 
 // Freeze the object that it cannot be modified during runtime.
